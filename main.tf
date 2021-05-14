bae9e49276fc0a995a5c556fd5b60cde81d470f9
aniruddha-git143 <aniruddha.nd@gmail.com>
2021-05-14 12:32:13 +0100

    Adding code for ec2 imds v2 enablement

--- main.tf
+++ main.tf
@@ -116,6 +116,13 @@ resource aws_launch_configuration "conf" {
     virtual_name = "ephemeral0"
   }
 
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+
   lifecycle {
     create_before_destroy = true
   }
